9b6eb2dcbc8c35a2d3367e1f1214d0e9d9db2612
Vivek Moar <44374277+vivekiitm@users.noreply.github.com>
2020-07-03 10:53:15 +0530

    origin

--- playBingoApp/app/src/main/java/com/example/playbingo/onlinegame.java
+++ playBingoApp/app/src/main/java/com/example/playbingo/onlinegame.java
@@ -38,42 +38,21 @@ public class onlinegame extends AppCompatActivity implements View.OnClickListene
         } catch (URISyntaxException e) {}
     }
 
-
-    // Function to return the next random number
     private static int getNum(ArrayList<Integer> v)
     {
-        // Size of the vector
         int n = v.size();
-
-        // Make sure the number is within
-        // the index range
         int index = (int)(Math.random() * n);
-
-        // Get random number from the vector
         int num = v.get(index);
-
-        // Remove the number from the vector
         v.set(index, v.get(n - 1));
         v.remove(n - 1);
-
-        // Return the removed number
         return num;
     }
 
-    // Function to generate n
-    // non-repeating random numbers
     private static void generateRandom(int n,ArrayList<Integer> vv)
     {
-
-        // Fill the vector with the values
-        // 1, 2, 3, ..., n
-
         ArrayList<Integer> v = new ArrayList<>(n);
         for (int i = 0; i < n; i++)
             v.add(i + 1);
-
-        // While vector has elements
-        // get a random number from the vector and print it
         while (v.size() > 0)
         {
             vv.add(getNum(v));
